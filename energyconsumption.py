614b6770d182ffb720a1af090657302b297b67b0
Michael Troger <11340859+michaeltroger@users.noreply.github.com>
2025-07-27 16:01:36 +0200

    Update energyconsumption.py

--- energyconsumption.py
+++ energyconsumption.py
@@ -1,9 +1,11 @@
 import imaplib, email, io, csv
 import hassapi as hass
+from email.header import decode_header, make_header
 
 class EnergyConsumption(hass.Hass):
     def initialize(self):
         self.log("Hello from Energy Consumption App")
+        self.check_email({})
         self.run_every(self.check_email, "now", 3600)
 
     def check_email(self, kwargs):
@@ -16,10 +18,12 @@ class EnergyConsumption(hass.Hass):
             self.log("found email")
             _, msg_data = mail.fetch(num, "(RFC822)")
             msg = email.message_from_bytes(msg_data[0][1])
+            self.log(f"Subject: {str(make_header(decode_header(msg['Subject'])))}")
             for part in msg.walk():
                 ct = part.get_content_maintype()
                 if part.get_filename() and part.get_filename().lower().endswith(".csv"):
                     self.log("found csv")
+                    self.log(f"Filename: {part.get_filename()}")
                     body = part.get_payload(decode=True)
                     reader = csv.DictReader(io.StringIO(body.decode()), delimiter=";")
                     total = 0.0
@@ -35,6 +39,12 @@ class EnergyConsumption(hass.Hass):
                         "input_number/set_value",
                         entity_id = "input_number.daily_energy",
                         value = total
+                    )
+                    self.log("resetting sensor again")
+                    self.call_service(
+                        "input_number/set_value",
+                        entity_id = "input_number.daily_energy",
+                        value = 0
                     )
             mail.store(num, '+FLAGS', '\\Seen')
         mail.logout()
